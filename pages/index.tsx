2b02756372bbfe40f4b7f5859fe43b3bd5b4afc1
にっく <n.ieqclab@gmail.com>
2025-08-20 00:47:19 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import InstagramFeed from "@/components/InstagramFeed";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>最新のInstagram投稿</h1>
+      <InstagramFeed />
+    </main>
+  );
+}
