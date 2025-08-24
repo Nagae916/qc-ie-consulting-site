8f3dc8d645f03b3b36a19a18822e398502a4b972
にっく <n.ieqclab@gmail.com>
2025-08-24 13:09:27 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app'
-import '../index.css'  // Tailwind 読み込み
+import type { AppProps } from "next/app";
+import "../index.css"; // ← Tailwind / グローバルCSS を読み込み
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
