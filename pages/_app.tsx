2763a97f8054f925d945559b97ee8a74931a5bfa
にっく <n.ieqclab@gmail.com>
2025-08-24 18:55:03 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app';
-import '../styles/globals.css'; // ← ここでグローバルCSSを読み込む
+import type { AppProps } from "next/app";
+import "../styles/globals.css"; // ← ここだけに集約（src/index.css は使わない）
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
