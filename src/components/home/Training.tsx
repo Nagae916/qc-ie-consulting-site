e10c276237a923c304e85d6c24fe46479cb1dfcc
にっく <n.ieqclab@gmail.com>
2025-08-25 12:27:37 +0900

    Create Training.tsx

---
+++ src/components/home/Training.tsx
@@ -0,0 +1,8 @@
+export default function Training() {
+  return (
+    <section className="mt-8">
+      <h2 className="text-xl font-semibold">研修（カスタム前提）</h2>
+      {/* SPC演習 / IE基礎 などの紹介 */}
+    </section>
+  );
+}
