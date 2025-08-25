313decb480971b1c8845c0e4f29f8220ae611ca7
にっく <n.ieqclab@gmail.com>
2025-08-25 22:02:29 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 // next.config.js
 const path = require("path");
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    // ここでランタイム解決も固定（TS だけに依存しない）
+    // 既存の @ エイリアスを維持
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);
