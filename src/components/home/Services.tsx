025a19af7aa59c4f8960c308c925497195d1f9aa
にっく <n.ieqclab@gmail.com>
2025-08-25 12:26:44 +0900

    Create Services.tsx

---
+++ src/components/home/Services.tsx
@@ -0,0 +1,9 @@
+export default function Services() {
+  return (
+    <section className="space-y-4">
+      <h2 className="text-xl font-semibold">学習サポート</h2>
+      {/* ここにカードUIなど共通部品を使って並べる */}
+      {/* QC検定 / 統計検定 / 技術士 など */}
+    </section>
+  );
+}
