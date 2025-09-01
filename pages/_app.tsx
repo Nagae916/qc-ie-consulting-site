38402406eea75f77a3e7d43e51b4a61937b2d88e
にっく <n.ieqclab@gmail.com>
2025-09-01 22:49:42 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
+import "katex/dist/katex.min.css";        // ★ 追加：KaTeX のスタイルを全ページに適用
 import "../styles/globals.css";
 import ErrorBoundary from "@/components/common/ErrorBoundary";
 
