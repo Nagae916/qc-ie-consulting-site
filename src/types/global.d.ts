033068eb94c8fe7f3fc861cd460dda588e15e692
にっく <n.ieqclab@gmail.com>
2025-09-15 16:06:25 +0900

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,14 @@
 // src/types/global.d.ts
-export {};
+
+/* eslint-disable unused-imports/no-unused-vars */
+/**
+ * グローバル Window の型拡張。
+ * eslint の「未使用変数」警告対象になるため、このファイルだけ無効化しています。
+ */
 declare global {
   interface Window {
     twttr?: any;
   }
 }
+
+export {};
